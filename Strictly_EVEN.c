e5af1bf65523ddd6c29764415d3030fae2fc0986
Gopalakrishna0001 <120792723+Gopalakrishna0001@users.noreply.github.com>
2022-12-29 11:15:07 +0530

    #include<stdio.h> int main() {     int N;     scanf("%d",&N);     int a[N],i,x;     for(i=0;i<N;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<N;i++)     {         if(a[i]%2==0)         {             if(i%2==0)             {                 x=0;             }             else             {                 x=1;                 break;             }         }     }     if(x==0)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Strictly_EVEN.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    int N;
+    scanf("%d",&N);
+    int a[N],i,x;
+    for(i=0;i<N;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<N;i++)
+    {
+        if(a[i]%2==0)
+        {
+            if(i%2==0)
+            {
+                x=0;
+            }
+            else
+            {
+                x=1;
+                break;
+            }
+        }
+    }
+    if(x==0)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}    
